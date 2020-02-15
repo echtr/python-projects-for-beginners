055a59c48c414ae9019274b3027a5c8929a1c5fd
Efe İ <41235050+EchTR@users.noreply.github.com>
2020-02-15 20:38:44 +0300

    Add files via upload

---
+++ who likes it.py
@@ -0,0 +1,14 @@
+# Github: EchTR
+def likes(names):
+	uzunluk = len(names)
+	if (uzunluk == 0):
+		return "no one likes this"
+	elif (uzunluk == 1):
+		return names[0] + " likes this"
+	elif (uzunluk == 2):
+		return names[0] + " and " + names[1] + " like this"
+	elif (uzunluk == 3):
+		return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+	elif (uzunluk >= 4):
+		return names[0] + ", " + names[1] + " and " + str(uzunluk - 2) + " others like this"
+input(likes(["Efe","Rüzgar","Tüzgar","Püzgar","Züzgar"]))
